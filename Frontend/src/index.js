bcf51abaa1cda41a1ec8617c42d0224655b8e710
Claudia Fox <claudiafox95@icloud.com>
2019-01-31 17:32:35 +0000

    first commit

---
+++ Frontend/src/index.js
@@ -0,0 +1,21 @@
+document.addEventListener( 'DOMContentLoaded', initialize)
+
+baseUrl = "http://localhost:3000/posts"
+
+function initialize() {
+  getPosts().then(posts => {
+    state.posts = posts
+    addPostsToPage(state.posts)
+  })
+}
+
+const state = {
+  posts: [],
+}
+
+const addPostsToPage = posts =>
+
+const getPosts = () => {
+  return fetch(baseUrl)
+    .then(response => response.json())
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dog-Instagram
+A single page application, in Ruby, JavaScript, HTML &amp; CSS. Ability to create and delete posts, like posts, and add, delete and edit comments. 
